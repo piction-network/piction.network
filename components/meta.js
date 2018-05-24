df2714625785b21e045cf65ce1f1370c4d4adc0d
Jiwoong Heo <jiwoong.heo@gmail.com>
2018-05-24 10:39:51 +0900

    Change project name

--- components/meta.js
+++ components/meta.js
@@ -3,11 +3,11 @@ import Head from "next/head";
 export default ({locale}) => (
   <Head>
     <meta charSet="utf-8" />
-    <title>Piction</title>
+    <title>Piction network</title>
     <meta name="description" content={locale.hero} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <meta name="og:title" content="Piction" />
+    <meta name="og:title" content="Piction network" />
     <meta name="og:description" content={locale.hero} />
     <meta name="og:image" content="/static/og.png" />
 
