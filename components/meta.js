ec1b11c94f100ecfa34ab04707345f575630cccf
Jiwoong Heo <jiwoong.heo@gmail.com>
2018-06-20 18:23:46 +0900

    Fix broken og meta description

--- components/meta.js
+++ components/meta.js
@@ -8,7 +8,7 @@ export default ({locale}) => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <meta name="og:title" content="Piction Protocol" />
-    <meta name="og:description" content={locale.hero} />
+    <meta name="og:description" content="Piction Protocol is creating a new peer to peer digital contents ecosystem. We are disrupting the centralized models of content distribution dominated by extractive intermediaries" />
     <meta name="og:image" content="/static/og.png" />
 
     <link rel="shortcut icon" href="/static/favicon.png" />
