f127e4b3a157216c78fbb77e4cc6ba29f5ddd5b6
Jiwoong Heo <jiwoong.heo@gmail.com>
2018-12-26 15:02:13 +0900

    Fix typo

--- components/sections/roadmap.js
+++ components/sections/roadmap.js
@@ -149,51 +149,59 @@ export default () => (
       </li>
       <li>
         <Milestone time="2019 Q2" title="beta v0.5 version release" side="left">
-          <li>
-            We intend to expand our ecosystem partnership, which has own unique market scope in each area and a large number of users. We intend to aggressively engage a large number of users who will participate in creators, funding, marketing, and translation in order to ensure that Piction Network operates successfully and successfully.
-          </li>
-          <li>
-            Geographically, Piction Network begins from Korea and China. Also, based on content category, it starts in the field of webtoon /web novel.
-          </li>
-          <li>
-            Piction Network beta version will be released on mainnet.
-          </li>
-          <li>
-            Base protocol will be determined, and SDK and API also will be announced.
-          </li>
+          <ul>
+            <li>
+              We intend to expand our ecosystem partnership, which has own unique market scope in each area and a large number of users. We intend to aggressively engage a large number of users who will participate in creators, funding, marketing, and translation in order to ensure that Piction Network operates successfully and successfully.
+            </li>
+            <li>
+              Geographically, Piction Network begins from Korea and China. Also, based on content category, it starts in the field of webtoon /web novel.
+            </li>
+            <li>
+              Piction Network beta version will be released on mainnet.
+            </li>
+            <li>
+              Base protocol will be determined, and SDK and API also will be announced.
+            </li>
+          </ul>
         </Milestone>
       </li>
       <li>
         <Milestone time="2019 Q3/Q4" title="v1.0 version release" side="right">
-          <li>
-            Piction Network will launch the full version that enables the entire content creation process, from content registration and supporter recruitment to sales.
-          </li>
-          <li>
-            We plan to concentrate production of secondary creative based on game IP. It is also proven through the experience and history of Battle Comics, which has accumulated 18 million users in Korea.
-          </li>
-          <li>
-            We aim to expand into other regions such as Japan, Southeast Asia.
-          </li>
+          <ul>
+            <li>
+              Piction Network will launch the full version that enables the entire content creation process, from content registration and supporter recruitment to sales.
+            </li>
+            <li>
+              We plan to concentrate production of secondary creative based on game IP. It is also proven through the experience and history of Battle Comics, which has accumulated 18 million users in Korea.
+            </li>
+            <li>
+              We aim to expand into other regions such as Japan, Southeast Asia.
+            </li>
+          </ul>
         </Milestone>
       </li>
       <li>
         <Milestone time="2020" side="left">
-          <li>
-            Ecosystem Expansion Phase, we plan to expand to the digital content market such as short clip movies. Video is the largest market for the type of content that Piction Network is planning, where fairly advanced plans are developed every day.
-          </li>
-          <li>
-            Physical region expansion will be focused on the United States and the EU.
-          </li>
+          <ul>
+            <li>
+              Ecosystem Expansion Phase, we plan to expand to the digital content market such as short clip movies. Video is the largest market for the type of content that Piction Network is planning, where fairly advanced plans are developed every day.
+            </li>
+            <li>
+              Physical region expansion will be focused on the United States and the EU.
+            </li>
+          </ul>
         </Milestone>
       </li>
       <li>
         <Milestone time="2021" side="right">
-          <li>
-            We target expanding to the music market, which is the most popular and beloved content.
-          </li>
-          <li>
-            Physical regional expansion will be focus on other underdeveloped areas such as Latin America and Africa. After 2021, the world becomes the service area of Piction Network.
-          </li>
+          <ul>
+            <li>
+              We target expanding to the music market, which is the most popular and beloved content.
+            </li>
+            <li>
+              Physical regional expansion will be focus on other underdeveloped areas such as Latin America and Africa. After 2021, the world becomes the service area of Piction Network.
+            </li>
+          </ul>
         </Milestone>
       </li>
     </ul>
